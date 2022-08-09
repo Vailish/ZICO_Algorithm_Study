9493cc79f8a1ef110e8b0abc7c0ff0e1f829722f
vailish <joyksj94@gmail.com>
2022-08-10 08:24:56 +0900

    add pb

---
+++ 김성준/2주차(2022.08.08~14)/swea_2046.py
@@ -0,0 +1,5 @@
+#2046. 스탬프 찍기 D1
+#3https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKdT6AyYDFAUq&categoryId=AV5QKdT6AyYDFAUq&categoryType=CODE&problemTitle=2046&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1&&&&&&&&&
+
+num = int(input())
+print('#'*num)
---
+++ 김성준/2주차(2022.08.08~14)/swea_2047.py
@@ -0,0 +1,5 @@
+#2043. 서랍의 비밀번호 D1
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QJ_8KAx8DFAUq&categoryId=AV5QJ_8KAx8DFAUq&categoryType=CODE&problemTitle=2043&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+
+P, K = map(int, input().split())
+print(P-K+1)
