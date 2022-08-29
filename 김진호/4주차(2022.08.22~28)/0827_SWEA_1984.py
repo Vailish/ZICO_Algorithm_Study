e39e2d0773546193a5d0cc2c38bb1209ba9d9175
kimjinho-dev <kimjinho.dev@gmail.com>
2022-08-29 23:40:44 +0900

    20220829_study_after

--- 김진호/4주차(2022.08.22~28)/0827_SWEA_1984.py
+++ 김진호/4주차(2022.08.22~28)/0827_SWEA_1984.py
@@ -1,4 +1,4 @@
-for test_case in range(1,int(input())+1):
+for test_case in range(1, int(input())+1):
     nums = list(map(int, input().split()))
     nums.sort()
     result = round(sum(nums[1:9]) / 8)
